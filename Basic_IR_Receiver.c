bdc7e976e6d61beb31f86a78744f862a2e8870c8
Brad Henson <hensonbl@gmail.com>
2017-10-31 10:42:49 -0400

    Add files via upload

--- Basic_IR_Receiver.c
+++ Basic_IR_Receiver.c
@@ -12,20 +12,22 @@
 #include <avr/io.h>				// included in all avr projects
 #include <util/delay.h>			// add this to use the delay function
 #include <avr/interrupt.h>		// add this to use the interrupt function
+
 uint8_t inputBuffer[44];		// creates a buffer array for storing input values
 uint8_t readingOne[10];
 uint8_t readingTwo[10];
 uint8_t readingThree[10];
 uint8_t totalReadings = 0;		// keeps track of how many readings have been received
-uint8_t x;
-uint8_t y;
+uint8_t k = 0;
+uint8_t i = 0;
 uint8_t finalRoute = 0;
+uint8_t headerGood = 1;			// flag
+uint8_t readingMatch = 1;		// flag
+uint8_t compareStatus = 0;		// flag
+uint8_t readyStatus = 0;		// flag
 int convertedRoute = 0;
-bool headerGood = false;
-bool readingMatch = true;
-bool compareStatus = false;
-bool readyStatus = false;
-void compareinputbuffer(uint8_t value);
+
+void compareinputbuffer();
 void cleareverything();
 
 int main(void)
@@ -44,96 +46,92 @@ int main(void)
     {
 		if (totalReadings == 3)
 		{
-			EIMSK =& ~(1 << 0);
-			uint8_t i = 0;
-			do(i < 10)
+			EIMSK &= ~(1 << 0);
+			i = 0;
+			while(i < 10)
 			{				
 				if (readingOne[i] == readingTwo[i] && readingTwo[i] == readingThree[i])
 				{
-					readingMatch = true;
+					readingMatch = 1;
 				}
 				else
 				{
-					cleareverything();
+					for (i = 0; i < 10; i++) readingOne[i] = 0;
+					for (i = 0; i < 10; i++) readingTwo[i] = 0;
+					for (i = 0;	i < 10; i++) readingThree[i] = 0;
+					readingMatch = 0;
+					totalReadings = 0;
+					compareStatus = 0;
+					readyStatus = 0;
 					break;
 				}
 				i++;
 			}
-			if(readingMatch == true && i == 10) 
+			if(readingMatch == 1 && i == 10) 
 			{
-				for (uint8_t k = 0; k < 16; k++);
+				for (k = 0; k < 16; k++);
 				{
 					if (readingOne[k] == 0)
 					{
-						convertedRoute =& ~(1 = k);
+						convertedRoute &= ~(1 << k);
 					}
 					else
 					{
-						convertedRoute =| (1 = k);
+						convertedRoute |= (1 << k);
 					}
 				}
 				switch (convertedRoute)
 				{
-					case 0b0000000001010101:
+				case 0b0000000001010101:
 					finalRoute = 0b00000000;	// route 1 - 000
-					compareStatus = true;
+					compareStatus = 1;
 					break;
-				}
 				case 0b0000000010101001:
-				{
 					finalRoute = 0b00000001;	// route 2 - 001
-					compareStatus = true;
+					compareStatus = 1;
 					break;
-				}
 				case 0b0000000010100101:
-				{
 					finalRoute = 0b00000010;	// route 3 - 010
-					compareStatus = true;
+					compareStatus = 1;
 					break;
-				}
 				case 0b0000000101001001:
-				{
 					finalRoute = 0b00000011;	// route 4 - 011
-					compareStatus = true;
+					compareStatus = 1;
 					break;
-				}
 				case 0b0000000010010101:
-				{
 					finalRoute = 0b00000100;	// route 5 - 100
-					compareStatus = true;
+					compareStatus = 1;
 					break;
-				}
 					case 0b0000000100101001:
-				{
 					finalRoute = 0b00000101;	// route 6 - 101
-					compareStatus = true;
+					compareStatus = 1;
 					break;
-				}
 				case 0b0000000100100101:
-				{
 					finalRoute = 0b00000110;	// route 7 - 110
-					compareStatus = true;
+					compareStatus = 1;
 					break;
-				}
 				case 0b0000001001001001:
-				{
 					finalRoute = 0b00000111;	// route 8 - 111
-					compareStatus = true;
+					compareStatus = 1;
 					break;
 				}
-						
-				if (compareStatus == true)
+				if (compareStatus == 1)
 				{
-					readyStatus = true;
+					readyStatus = 1;
 					// need to add code here to display the final route
 					// on a display
 				} 
 				else
 				{
-					readyStatus = false;
-					cleareverything();
+					for (i = 0; i < 10; i++) readingOne[i] = 0;
+					for (i = 0; i < 10; i++) readingTwo[i] = 0;
+					for (i = 0; i < 10; i++) readingThree[i] = 0;
+					totalReadings = 0;
+					compareStatus = 0;
+					readyStatus = 0;
+					readingMatch = 0;
 				}
-			break;				
+				break;				
 			}
 		}
 	/////////////////////////////////////////////////////////////////////////
@@ -145,68 +143,153 @@ int main(void)
 	// add code here to start moving the vehicle
 	
 	}	// end of while(1) loop
-
+}
 /////////////////////////////////////////////////////////////////////////////
 
 	ISR(INT0_vect)
 	{
-		
 		_delay_us(230);
 		inputBuffer[0] = PIND2;
 				
-		for (uint8_t i = 1; i < 44, i++)
+		for (i = 1; i < 44; i++)
 		{
 			_delay_us(560);
 			inputBuffer[i] = PIND2;
 		}
 		for (i = 0; i < 16; i++)
 		{
-			compareinputbuffer(i,1);
+			if (inputBuffer[i] == 1)
+			{
+				headerGood = 1;
+			}
+			else
+			{
+				headerGood = 0;
+				break;
+			}
 		}
 		
 		for (i = 15; i < 24; i++)
 		{
-			if (headerGood == false)
+			if (headerGood == 0)
 			{
 				break;
 			}
-			compareinputbuffer(i,0);
+			if (inputBuffer[i] == 0)
+			{
+				headerGood = 1;
+			}
+			else
+			{
+				headerGood = 0;
+			}
 		}		
 		
 		i = 24;
-		do (headerGood == true && int < 36)
+		
+		while(headerGood == 1 && i < 36)
 		{
 			 switch (i)
 			 {
 				 case 0:
-					compareinputbuffer(i,1);				 
+					if (inputBuffer[i] == 1)
+					{
+						headerGood = 1;
+					}
+					else
+					{
+						headerGood = 0;
+					}				 
 					break;				 
 				 case 1:
-					compareinputbuffer(i,0);
+					if (inputBuffer[i] == 0)
+					{
+						headerGood = 1;
+					}
+					else
+					{
+						headerGood = 0;
+					}
 					break;
 				 case 2:
-					compareinputbuffer(i,1);				 
+					if (inputBuffer[i] == 1)
+					{
+						headerGood = 1;
+					}
+					else
+					{
+						headerGood = 0;
+					}				 
 					break;
 				 case 3:
-					compareinputbuffer(i,0);				 
+					if (inputBuffer[i] == 0)
+					{
+						headerGood = 1;
+					}
+					else
+					{
+						headerGood = 0;
+					}				 
 					break;
 				 case 4:
-					compareinputbuffer(i,1);				 
+					if (inputBuffer[i] == 1)
+					{
+						headerGood = 1;
+					}
+					else
+					{
+						headerGood = 0;
+					}				 
 					break;
 				 case 5:
-					compareinputbuffer(i,0);				 
+					if (inputBuffer[i] == 0)
+					{
+						headerGood = 1;
+					}
+					else
+					{
+						headerGood = 0;
+					}				 
 					break;	
 				 case 6:
-					compareinputbuffer(i,1);				 
+					if (inputBuffer[i] == 1)
+					{
+						headerGood = 1;
+					}
+					else
+					{
+						headerGood = 0;
+					}				 
 					break;
 				 case 7:
-					compareinputbuffer(i,0);				 
+					if (inputBuffer[i] == 0)
+					{
+						headerGood = 1;
+					}
+					else
+					{
+						headerGood = 0;
+					}				 
 					break;
 				 case 8:
-					compareinputbuffer(i,1);				 
+					if (inputBuffer[i] == 1)
+					{
+						headerGood = 1;
+					}
+					else
+					{
+						headerGood = 0;
+					}				 
 					break;
 				 case 9:
-					compareinputbuffer(i,0);				 
+					if (inputBuffer[i] == 0)
+					{
+						headerGood = 1;
+					}
+					else
+					{
+						headerGood = 0;
+					}				 
 					break;
 				 case 10:
 					for (i = 35; i < 45; i++)
@@ -242,29 +325,3 @@ int main(void)
 		
 		}
 	}
-/////////////////////////////////////////////////////////////////////////////
- 
-	void compareinputbuffer(uint8_t x, uint8_t y)
-	{
-		if (inputBuffer[x] == y)
-		{
-			headerGood = true;
-		}
-		else
-		{
-			headerGood = false;
-			break;
-		}
-	return;
-	}
-	
-	void cleareverything()
-	{
-		readingMatch = false;
-		readingOne[] = {0,0,0,0,0,0,0,0,0,0};
-		readingTwo[] = {0,0,0,0,0,0,0,0,0,0};
-		readingThree[] = {0,0,0,0,0,0,0,0,0,0};
-		totalReadings = 0;
-	}
-
-}
